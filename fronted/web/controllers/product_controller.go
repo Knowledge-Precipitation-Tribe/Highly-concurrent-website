aeb9cfc363d7df13a6bf946b48bd26f15fcb9f63
super <supeng842499467@gmail.com>
2020-06-23 21:05:19 +0800

    gen static

--- fronted/web/controllers/product_controller.go
+++ fronted/web/controllers/product_controller.go
@@ -6,7 +6,10 @@ import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
 	"github.com/kataras/iris/sessions"
+	"os"
+	"path/filepath"
 	"strconv"
+	"text/template"
 )
 
 type ProductController struct {
@@ -16,21 +19,72 @@ type ProductController struct {
 	Session        *sessions.Session
 }
 
+var (
+	//生成的Html保存目录
+	htmlOutPath = "./fronted/web/htmlProductShow/"
+	//静态文件模版目录
+	templatePath = "./fronted/web/views/template/"
+)
+
+func (p *ProductController) GetGenerateHtml() {
+	productString := p.Ctx.URLParam("productID")
+	productID,err:=strconv.Atoi(productString)
+	if err !=nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	//1.获取模版
+	contenstTmp,err:=template.ParseFiles(filepath.Join(templatePath,"product.html"))
+	if err !=nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	//2.获取html生成路径
+	fileName:=filepath.Join(htmlOutPath,"htmlProduct.html")
+
+	//3.获取模版渲染数据
+	product,err:=p.ProductService.GetProductByID(int64(productID))
+	if err !=nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+	//4.生成静态文件
+	generateStaticHtml(p.Ctx,contenstTmp,fileName,product)
+}
+
+//生成html静态文件
+func generateStaticHtml(ctx iris.Context,template *template.Template,fileName string,product *datamodels.Product)  {
+	//1.判断静态文件是否存在
+	if exist(fileName) {
+		err:=os.Remove(fileName)
+		if err !=nil {
+			ctx.Application().Logger().Error(err)
+		}
+	}
+	//2.生成静态文件
+	file,err := os.OpenFile(fileName,os.O_CREATE|os.O_WRONLY,os.ModePerm)
+	if err !=nil {
+		ctx.Application().Logger().Error(err)
+	}
+	defer file.Close()
+	template.Execute(file,&product)
+}
+
+//判断文件是否存在
+func exist(fileName string) bool  {
+	_,err:=os.Stat(fileName)
+	return err==nil || os.IsExist(err)
+}
 
 func (p *ProductController) GetDetail() mvc.View {
-	//id := p.Ctx.URLParam("productID")
-	//fmt.Println("product_id", id)
-	//这里暂时写死ß
-	product, err := p.ProductService.GetProductByID(14)
-	if err != nil {
+	product,err:=p.ProductService.GetProductByID(1)
+	if err !=nil {
 		p.Ctx.Application().Logger().Error(err)
 	}
 
 	return mvc.View{
-		Layout: "shared/productLayout.html",
-		Name:   "product/view.html",
-		Data: iris.Map{
-			"product": product,
+		Layout:"shared/productLayout.html",
+		Name:"product/view.html",
+		Data:iris.Map{
+			"product":product,
 		},
 	}
 }
